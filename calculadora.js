b4c5b8b595a6891b378930b8d48e923d82c25233
Kenji Sakai <114245591+kenjisakai-dev@users.noreply.github.com>
2026-07-12 16:08:46 -0300

    Initial commit

---
+++ calculadora.js
@@ -0,0 +1,30 @@
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+function resto(a, b) {
+    return a % b;
+}
+
+function potencia(a, b) {
+    return a ** b;
+}
+
+console.log(somar(2, 5));
+console.log(subtrair(5, 2));
+console.log(multiplicar(5, 5));
+console.log(dividir(10, 2));
+console.log(resto(10, 3));
+console.log(potencia(2, 5));
